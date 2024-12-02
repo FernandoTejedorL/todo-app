7bdae0d20eb4de42ed077f145da8418721bb8cb7
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-12-02 14:15:47 +0100

    truly completed

--- js/scripts.js
+++ js/scripts.js
@@ -13,13 +13,13 @@ let tasks = [];
 
 //Cambiar entre modos
 
-const toDarkMode = (event) => {
+const toDarkMode = () => {
   document.body.classList.add('dark');
   deactivateDarkElement.classList.remove('hide');
   activateDarkElement.classList.add('hide');
 };
 
-const toNormalMode = (event) => {
+const toNormalMode = () => {
   document.body.classList.remove('dark');
   deactivateDarkElement.classList.add('hide');
   activateDarkElement.classList.remove('hide');
@@ -28,84 +28,47 @@ const toNormalMode = (event) => {
 // imprimir tareas
 
 const printTasks = (tasks) => {
+  const fragment = document.createDocumentFragment();
+  toAddTasksElement.textContent = '';
   tasks.forEach((task) => {
-    if (task.completed === true) {
-      const fragment = document.createDocumentFragment();
+    const circle = document.createElement('div');
+    circle.classList.add('circle', 'pointer-none');
+    circle.dataset.classToAdd = 'circle';
 
-      const check = document.createElement('img');
-      check.classList.add('check');
-      check.src = './assets/images/icon-check.svg';
+    const taskText = document.createElement('span');
+    taskText.textContent = task.name;
+    taskText.classList.add('pointer-none');
+    circle.dataset.classToAdd = 'span';
 
-      const circle = document.createElement('div');
-      circle.classList.add('circle', 'pointer-none', 'circle-completed');
-      circle.dataset.classToAdd = 'circle';
+    const taskCreated = document.createElement('div');
+    taskCreated.classList.add('task-created');
+    taskCreated.dataset.type = 'toComplete';
+    taskCreated.dataset.identificator = task.id;
 
-      const taskText = document.createElement('span');
-      taskText.textContent = task.name;
-      taskText.classList.add('pointer-none', 'txt-completed');
-      circle.dataset.classToAdd = 'span';
+    const deleteItem = document.createElement('img');
+    deleteItem.classList.add('delete-item', 'delete-item-hide');
+    deleteItem.src = './assets/images/icon-cross.svg';
+    deleteItem.dataset.type = 'delete';
+    deleteItem.dataset.identificator = task.id;
+    deleteItem.addEventListener('click', () => clearTask(event));
 
-      const taskCreated = document.createElement('div');
-      taskCreated.classList.add('task-created');
-      taskCreated.dataset.type = 'markCompleted';
-      taskCreated.dataset.identificator = task.id;
-
-      const deleteItem = document.createElement('img');
-      deleteItem.classList.add('delete-item', 'delete-item-hide');
-      deleteItem.src = './assets/images/icon-cross.svg';
-      deleteItem.dataset.type = 'delete';
-      deleteItem.dataset.identificator = task.id;
-
-      const newTask = document.createElement('div');
-      newTask.classList.add('new-task');
-
-      circle.append(check);
-      taskCreated.append(circle, taskText);
-      newTask.append(taskCreated, deleteItem);
-      fragment.append(newTask);
-      toAddTasksElement.append(fragment);
-    } else if (task.completed === false) {
-      const fragment = document.createDocumentFragment();
-
-      const check = document.createElement('img');
-      check.classList.add('check');
-      check.src = './assets/images/icon-check.svg';
-
-      const circle = document.createElement('div');
-      circle.classList.add('circle', 'pointer-none');
-
-      const taskText = document.createElement('span');
-      taskText.textContent = task.name;
-      taskText.classList.add('pointer-none');
-
-      const taskCreated = document.createElement('div');
-      taskCreated.classList.add('task-created');
-      taskCreated.dataset.type = 'markCompleted';
-      taskCreated.dataset.identificator = task.id;
-
-      const deleteItem = document.createElement('img');
-      deleteItem.classList.add('delete-item', 'delete-item-hide');
-      deleteItem.src = './assets/images/icon-cross.svg';
-      deleteItem.dataset.type = 'delete';
-      deleteItem.dataset.identificator = task.id;
-
-      const newTask = document.createElement('div');
-      newTask.classList.add('new-task');
-
-      circle.append(check);
-      taskCreated.append(circle, taskText);
-      newTask.append(taskCreated, deleteItem);
-      fragment.append(newTask);
-      toAddTasksElement.append(fragment);
+    const newTask = document.createElement('div');
+    newTask.classList.add('new-task');
+    if (task.completed) {
+      taskText.classList.add('txt-completed');
+      circle.classList.add('circle-completed');
     }
+    taskCreated.append(circle, taskText);
+    newTask.append(taskCreated, deleteItem);
+    fragment.append(newTask);
+    toAddTasksElement.append(fragment);
   });
 };
 
 //Añadir tarea
 
-const addTask = (event) => {
+const addTask = () => {
   event.preventDefault();
-  toAddTasksElement.textContent = '';
   const textToAdd = event.target.children.input.value;
   const taskToCreate = {
     id: Date.now(),
@@ -116,39 +79,19 @@ const addTask = (event) => {
   printTasks(tasks);
   updateCounter(tasks);
   inputElement.value = '';
-  console.log(tasks);
 };
 
-//clicks para marcar completado
+//clicks para marcar/desmarcar completado
 
-const completeTask = (event) => {
+const toggleComplete = () => {
   const id = Number(event.target.dataset.identificator);
-  const circle = event.target.children[0];
-  const text = event.target.children[1];
-  tasks.forEach((task) => {
-    if (task.id === id) {
-      task.completed = true;
-      event.target.dataset.type = 'markUncompleted';
-      circle.classList.add('circle-completed');
-      text.classList.add('txt-completed');
-    }
-  });
-};
-
-// para desmarcar el completado
-
-const uncompleteTask = (event) => {
-  const id = Number(event.target.dataset.identificator);
-  const circle = event.target.children[0];
-  const text = event.target.children[1];
-  tasks.forEach((task) => {
-    if (task.id === id) {
-      task.completed = false;
-      event.target.dataset.type = 'markCompleted';
-      circle.classList.remove('circle-completed');
-      text.classList.remove('txt-completed');
-    }
-  });
+  const taskFound = tasks.find((task) => task.id === id);
+  if (taskFound.completed) {
+    taskFound.completed = false;
+  } else {
+    taskFound.completed = true;
+  }
+  printTasks(tasks);
 };
 
 //contador de completados
@@ -160,26 +103,21 @@ const updateCounter = (array) => {
 
 //filtrar activas
 
-const filterActive = (event) => {
-  let activeTasks = [...tasks];
-  activeTasks = activeTasks.filter((task) => task.completed === false);
-  toAddTasksElement.textContent = '';
+const filterActive = () => {
+  const activeTasks = tasks.filter((task) => !task.completed);
   printTasks(activeTasks);
 };
 
 //filtrar completadas
 
-const filterCompleted = (event) => {
-  let completedTasks = [...tasks];
-  completedTasks = completedTasks.filter((task) => task.completed === true);
-  toAddTasksElement.textContent = '';
+const filterCompleted = () => {
+  completedTasks = tasks.filter((task) => task.completed);
   printTasks(completedTasks);
 };
 
 //filtrar todas
 
-const filterAll = (event) => {
-  toAddTasksElement.textContent = '';
+const filterAll = () => {
   printTasks(tasks);
 };
 
@@ -196,18 +134,16 @@ const changeFilterColor = (event) => {
 
 //limpiar completos
 
-const clearCompleted = (event) => {
-  tasks = tasks.filter((task) => task.completed === false);
-  toAddTasksElement.textContent = '';
+const clearCompleted = () => {
+  tasks = tasks.filter((task) => !task.completed);
   printTasks(tasks);
 };
 
 //eliminar tarea
 
-const clearTask = (event) => {
+const clearTask = () => {
   const id = Number(event.target.dataset.identificator);
   tasks = tasks.filter((task) => task.id !== id);
-  toAddTasksElement.textContent = '';
   printTasks(tasks);
 };
 
@@ -216,11 +152,8 @@ const clearTask = (event) => {
 const clickInMain = (event) => {
   const type = event.target.dataset.type;
   if (!type) return;
-  if (type === 'markCompleted') {
-    completeTask(event);
-  }
-  if (type === 'markUncompleted') {
-    uncompleteTask(event);
+  if (type === 'toComplete') {
+    toggleComplete(event);
   }
   if (type === 'active') {
     filterActive(event);
@@ -233,9 +166,6 @@ const clickInMain = (event) => {
   }
   if (type === 'clear-completed') {
     clearCompleted(event);
-  }
-  if (type === 'delete') {
-    clearTask(event);
   }
   updateCounter(tasks);
   console.log(tasks);
